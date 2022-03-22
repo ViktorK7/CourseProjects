7c5be89f478126c1ec677b34eab9e6c7d29ace1a
Viktor Hadzhiev <91834394+ViktorK7@users.noreply.github.com>
2022-03-22 17:12:31 +0200

    Delete PHP/course-project directory

--- PHP/course-project/home/comments/delete.php
+++
@@ -1,7 +0,0 @@
-<?php
-     include "../../base.php";
-     $ticketId = $_GET['ticketId'];
-     $commentId = $_GET['commentId'];
-     $database->deleteComment($commentId);
-     header("Location: comments.php?ticketId=" . $ticketId);
-?>
--- PHP/course-project/home/delete.php
+++
@@ -1,6 +0,0 @@
-<?php
-     include "../base.php";
-     $ticketId = $_GET['ticketId'];
-     $database->deleteTicket($ticketId);
-     header("Location: tickets.php");
-?>
